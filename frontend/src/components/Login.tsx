123149d21dc52726e0e7b2e8841167543f723137
VuongNhatHien <hienvuongnhat@gmail.com>
2024-10-21 23:29:49 +0700

    Add nav bar in signIn and signUp

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import getURL from "../../utils/getURL";
 import { FloatingLabel, Form } from "react-bootstrap";
+import NavBar from "./NavBar";
 
 /* eslint-disable jsx-a11y/anchor-is-valid */
 export default function Login() {
@@ -31,62 +32,66 @@ export default function Login() {
   };
 
   return (
-    <div
-      className="d-flex justify-content-center align-items-center vh-100"
-      style={{
-        backgroundColor: "#eceff1",
-      }}
-    >
-      <Form
-        className="p-4 rounded w-25 bg-white shadow"
-        onSubmit={handleSubmit}
+    <>
+      <NavBar />
+      <div
+        className="d-flex justify-content-center align-items-center"
+        style={{
+          backgroundColor: "#eceff1",
+          height: "93vh",
+        }}
       >
-        <h3 className="text-center mb-3">Sign In</h3>
-        <FloatingLabel
-          className="mb-3"
-          label="Username or E-mail"
-          style={{
-            color: "#666666",
-          }}
+        <Form
+          className="p-4 rounded w-25 bg-white shadow"
+          onSubmit={handleSubmit}
         >
-          <Form.Control
-            required
-            type="text"
-            placeholder="Username or E-mail"
-            onChange={(e) => setUsernameOrEmail(e.target.value)}
-          />
-        </FloatingLabel>
+          <h3 className="text-center mb-3">Sign In</h3>
+          <FloatingLabel
+            className="mb-3"
+            label="Username or E-mail"
+            style={{
+              color: "#666666",
+            }}
+          >
+            <Form.Control
+              required
+              type="text"
+              placeholder="Username or E-mail"
+              onChange={(e) => setUsernameOrEmail(e.target.value)}
+            />
+          </FloatingLabel>
 
-        <FloatingLabel
-          className="mb-3"
-          label="Password"
-          style={{
-            color: "#666666",
-          }}
-        >
-          <Form.Control
-            required
-            type="password"
-            placeholder="Password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </FloatingLabel>
+          <FloatingLabel
+            className="mb-3"
+            label="Password"
+            style={{
+              color: "#666666",
+            }}
+          >
+            <Form.Control
+              required
+              type="password"
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </FloatingLabel>
 
-        <div className="mb-2">
-          <button type="submit" className="btn btn-primary w-100">
-            Sign In
-          </button>
-        </div>
+          <div className="mb-2">
+            <button type="submit" className="btn btn-primary w-100">
+              Sign In
+            </button>
+          </div>
 
-        <div className="d-flex justify-content-between align-items-center">
-          <p>
-            <Link to="/accounts/password/reset">Forgot Password?</Link>
-          </p>
-          <p>
-            <Link to="/accounts/register">Sign Up</Link>
-          </p>
-        </div>
-      </Form>
-    </div>
+          <div className="d-flex justify-content-between align-items-center">
+            <p>
+              <Link to="/accounts/password/reset">Forgot Password?</Link>
+            </p>
+            <p>
+              <Link to="/accounts/register">Sign Up</Link>
+            </p>
+          </div>
+        </Form>
+      </div>
+    </>
   );
 }
