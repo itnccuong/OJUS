ad07a091499abb1205df3682b1985e7091603f32
unknown <22120082@student.hcmus.edu.vn>
2024-11-28 16:25:06 +0700

    Add userId property to JwtPayload interface

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,17 +1,51 @@
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import { NavLink, useNavigate } from "react-router-dom";
 import getToken from "../../utils/getToken.ts";
+import getURL from "../../utils/getURL";
 import storageKeyMap from "../../utils/storageKeyMap.ts";
+import { jwtDecode, JwtPayload } from "jwt-decode";
 
 function NavBar() {
   const navigate = useNavigate();
-  const token = getToken(); // Get token from localStorage
+  const [username, setUsername] = useState("");
+  const token = getToken();
+
+  let ID = null;
+  if (token) {
+    try {
+      const decoded = jwtDecode(token) as JwtPayload & { userId: number };  // Cast the type
+
+      // Log all the decoded data to the console
+      ID = decoded.userId;
+    } catch (error) {
+      console.log("Error decoding token:", error);
+    }
+  }
+
+  const getUser = async () => {
+    try {
+      const { data } = await axios.get(getURL(`/api/user/${ID}`));
+      setUsername(data.data.user.username);
+      console.log("getprofile", data);
+    } catch (error: any) {
+      console.error(error);
+    }
+  };
+
   // Initialize navigate
   const handleSignOut = () => {
     localStorage.removeItem(storageKeyMap.token); // Remove token from localStorage
     navigate("/accounts/login"); // Redirect to login
   };
+
+  useEffect(() => {
+    getUser();
+    console.log("Just console to get rid of warning", token);
+  }, []);
+
   return (
     <Navbar className="bg-body-tertiary border-bottom d-flex">
       <div className="d-flex container">
@@ -50,7 +84,7 @@ function NavBar() {
             <>
               <Nav>
                 {/*Use token to get username, for now this is hardcoded*/}
-                <Nav.Link as={NavLink} to={`/u/duygay`}>
+                <Nav.Link as={NavLink} to={`/u/${username}`}>
                   Profile
                 </Nav.Link>
               </Nav>
