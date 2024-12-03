b10e3bbc49bfa8f472a64315b18db3574804ab87
VuongNhatHien <hienvuongnhat@gmail.com>
2024-12-04 01:46:51 +0700

    Kill 3308 container before testing to fix github action

--- backend/__test__/globalSetup.ts
+++ backend/__test__/globalSetup.ts
@@ -6,6 +6,17 @@ const execPromise = util.promisify(exec);
 
 module.exports = async () => {
   console.log("Initializing resources...");
+  // Clean up any existing containers
+  const { stdout } = await execPromise(
+    "docker ps -q --filter name=backend-mysql_test-1",
+  );
+  if (stdout.trim()) {
+    await execPromise("docker rm -f backend-mysql_test-1");
+  }
+
+  await execPromise("docker compose up -d");
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  await execPromise("dotenv -e .env.test -- prisma migrate dev");
   await cleanDatabase();
   await execPromise("ts-node prisma/seed.ts");
 };
