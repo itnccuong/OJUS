41211dc58c3070599f95709fe5a777f8cdfbcbe1
VuongNhatHien <hienvuongnhat@gmail.com>
2024-12-08 21:09:31 +0700

    Divide docker compose into 2 files

--- backend/services/auth.services/login.service.ts
+++ backend/services/auth.services/login.service.ts
@@ -48,7 +48,7 @@ export const signToken = async (userId: number) => {
   const token = jwt.sign(
     { userId: userId }, // Payload
     process.env.JWT_SECRET as string, // Secret
-    { expiresIn: "12m" }, // Token expiration
+    { expiresIn: "180d" }, // Token expiration
   );
   return token;
 };
