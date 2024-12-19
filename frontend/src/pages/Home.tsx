c00c2ea997a11379bf7faf926346904455dae148
VuongNhatHien <hienvuongnhat@gmail.com>
2024-12-20 04:16:33 +0700

    Update responsive home and problem list

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <>
       {/*<NavBar />*/}
-      <div className="container min-vh-100">
+      <div className="flex-grow-1 px-5">
         <h1>Coming soon...</h1>
       </div>
     </>
