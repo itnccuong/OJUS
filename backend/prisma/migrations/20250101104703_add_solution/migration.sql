d172aa38a0d6b70b6d4ef264e392cf7fdc18cbf9
QDuy0082 <22120082@student.hcmus.edu.vn>
2025-01-02 09:10:18 +0700

    fix db testcase

--- backend/prisma/migrations/20250101104703_add_solution/migration.sql
+++ backend/prisma/migrations/20250101104703_add_solution/migration.sql
@@ -1,4 +1,4 @@
 -- AlterTable
-ALTER TABLE `problem` ADD COLUMN `langSolution` VARCHAR(191) NULL,
+ALTER TABLE `Problem` ADD COLUMN `langSolution` VARCHAR(191) NULL,
     ADD COLUMN `solution` TEXT NULL,
     ADD COLUMN `tutorial` TEXT NULL;
